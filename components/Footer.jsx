af07f83e29705c341e10427f8c7dd16312a19420
sunday odum <sundayodum@gmail.com>
2022-03-03 18:15:18 +0100

    pizzalist, pizzacard and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import Navbar from './Navbar'
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
-    footer
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID.THE  PIZZA, WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            1654 Rumuagholu Road #304.
+            <br /> Port Harcourt, 55900
+            <br /> (234)703434-7814
+          </p>
+          <p className={styles.text}>
+            2356 K. Laquie Rd #235.
+            <br /> Port Harcourt, 55900
+            <br /> (234)703434-7814
+          </p>
+          <p className={styles.text}>
+            1614 E. Erwin St #104.
+            <br /> Port Harcourt, 55900
+            <br /> (234)703434-7814
+          </p>
+          <p className={styles.text}>
+            1614 W. Caroll St #125.
+            <br /> Port Harcourt, 55900
+            <br /> (234)703434-7814
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Footer
+export default Footer;
